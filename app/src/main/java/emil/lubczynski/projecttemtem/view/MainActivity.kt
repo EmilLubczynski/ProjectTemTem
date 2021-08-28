ee745ef0bfd68eb79cc6eac3fc43054a91178bf7
Emil <emchuck1@gmail.com>
2021-08-28 09:41:47 +0100

    folder structure commit

--- app/src/main/java/emil/lubczynski/projecttemtem/view/MainActivity.kt
+++ app/src/main/java/emil/lubczynski/projecttemtem/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package emil.lubczynski.projecttemtem
+package emil.lubczynski.projecttemtem.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import emil.lubczynski.projecttemtem.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
